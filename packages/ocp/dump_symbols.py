62310196294a23d04458b602736b9401a27cd579
impermaxvict <35343365+impermaxvict@users.noreply.github.com>
2022-01-19 23:38:47 +0100

    Create the OCP package

---
+++ packages/ocp/dump_symbols.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import pathlib
+import subprocess
+
+OCCT_LIBRARY_DIR = pathlib.Path(sys.argv[1])
+
+toolkits = OCCT_LIBRARY_DIR.glob("libTK*.a")
+
+exported_symbols = set()
+
+for toolkit in sorted(toolkits):
+    result = subprocess.run(
+        ["nm", "--print-armap", toolkit],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    for line in result.stdout.decode("utf-8").splitlines():
+        line = line.strip()
+        if len(line) > 0 and line != "Archive index:":
+            if " in " in line:
+                exported_symbols.add(line.split(" in ")[0].strip())
+            else:
+                raise "Error"
+
+with open("symbols_mangled_emscripten.dat", "w") as fp:
+    fp.writelines(list(sorted(exported_symbols)))
---
+++ packages/ocp/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+import pathlib
+
+OCCT_ROOT = pathlib.Path("/src/pyodide/packages/occt/build/occt-V7_5_3p1/install/")
+
+
+def find_occt_libs():
+    libs = []
+    for lib in sorted((OCCT_ROOT / "lib").glob("lib*.a")):
+        # lib = lib.relative_to(OCCT_ROOT / "lib")
+        if lib.stem[3:] == "TKXMesh":
+            continue
+        # libs.append(lib.stem[3:])
+        libs.append(lib.resolve())
+    libs = list(map(str, libs))
+    print("OCCT libraries:")
+    print(libs)
+    return libs
+
+
+RAPIDJSON_PATH = pathlib.Path(
+    "/src/pyodide/packages/rapidjson/build/rapidjson-1.1.0/include/"
+)
+
+ext_modules = [
+    Pybind11Extension(
+        "OCP",
+        list(map(str, sorted(pathlib.Path("OCP").glob("*.cpp")))),
+        include_dirs=[
+            "OCP",
+            str(OCCT_ROOT / "include" / "opencascade"),
+            str(RAPIDJSON_PATH),
+        ],
+        library_dirs=[
+            str(OCCT_ROOT / "lib"),
+        ],
+        # libraries=find_occt_libs(),
+        extra_objects=find_occt_libs(),
+    ),
+]
+
+setup(
+    name="ocp",
+    version="7.5.3",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.6",
+)
